408811c68c451d4d2e8b88d8b0f59ae40922a9a4
dillardr <65522278+dillardr@users.noreply.github.com>
2021-09-19 18:53:26 -0700

    Formatted the ALU cases

--- CPU.v
+++ CPU.v
@@ -14,38 +14,22 @@ module alu(
     always @*
     begin
         case(ALU_Sel)
-            4'b0000:
-                ALU_Result = A + B;
-            4'b0001:
-                ALU_Result = A - B;
-            4'b0010:
-                ALU_Result = A * B;
-            4'b0011:
-                ALU_Result = A/B;
-            4'b0100: // Logical shift left
-                ALU_Result = A<<1;
-            4'b0101: // Logical shift right
-                ALU_Result = A>>1;
-            4'b0110: // Rotate left
-                ALU_Result = {A[6:0],A[7]};
-            4'b0111: // Rotate right
-                ALU_Result = {A[0],A[7:1]};
-            4'b1000: //  Logical and 
-                ALU_Result = A & B;
-            4'b1001: //  Logical or
-                ALU_Result = A | B;
-            4'b1010: //  Logical xor 
-                ALU_Result = A ^ B;
-            4'b1011: //  Logical nor
-                ALU_Result = ~(A | B);
-            4'b1100: // Logical nand 
-                ALU_Result = ~(A & B);
-            4'b1101: // Logical xnor
-                ALU_Result = ~(A ^ B);
-            4'b1110: // Greater comparison
-                ALU_Result = (A>B)?8'd1:8'd0 ;
-            4'b1111: // Equal comparison   
-                ALU_Result = (A==B)?8'd1:8'd0 ;
+            4'b0000: ALU_Result = A + B;
+            4'b0001: ALU_Result = A - B;
+            4'b0010: ALU_Result = A * B;
+            4'b0011: ALU_Result = A/B;
+            4'b0100: ALU_Result = A<<1;
+            4'b0101: ALU_Result = A>>1;
+            4'b0110: ALU_Result = {A[6:0],A[7]};
+            4'b0111: ALU_Result = {A[0],A[7:1]};
+            4'b1000: ALU_Result = A & B;
+            4'b1001: ALU_Result = A | B;
+            4'b1010: ALU_Result = A ^ B;
+            4'b1011: ALU_Result = ~(A | B);
+            4'b1100: ALU_Result = ~(A & B);
+            4'b1101: ALU_Result = ~(A ^ B);
+            4'b1110: ALU_Result = (A>B)?8'd1:8'd0 ;
+            4'b1111: ALU_Result = (A==B)?8'd1:8'd0 ;
             default: ALU_Result = A + B ; 
         endcase
     end
